ab9c3b8e1b4b2c0c0cc710888f753db87a87545d
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2023-05-25 22:36:35 +0900

    difflibを使った類似度のスコア付け

---
+++ src/compare_input.py
@@ -0,0 +1,21 @@
+import difflib
+
+
+def compare_input(input_data, database_data):
+    """
+    入力データとデータベース上の文字を比較する
+    compare with input data and output data.
+
+    parameters
+    ----------
+    input_data : address to be searched. 検索したい一つ住所
+    database_data : database address. データベース上の一つアドレス.
+
+    Returns
+    -------
+    score : similarity score between input_data and database_data.
+            input_dataとdatabase_dataの類似度スコア
+    """
+    score = difflib.SequenceMatcher(None, input_data, database_data).ratio()
+
+    return score
