40b3ac4ed4cb1197f0bd3e57d07799d3f3ee8ba7
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2023-05-25 23:14:56 +0900

    Update test_compare_input.py

--- test/test_compare_input.py
+++ test/test_compare_input.py
@@ -8,6 +8,23 @@ from compare_input import compare_input
 
 
 def test_compare_input():
+
++    """
++    This function tests the functionality of the compare_input function. It compares input_data
++    with database_data and checks if they are equal. If they are equal, it sets the score to 1.0
++    and asserts that the score is equal to expected_score. This function does not take any
++    parameters and does not return anything.
+
++    Compares two strings and returns a score of how similar they are.
++
++    Args:
++        input_data (str): A string to compare.
++        database_data (str): A string to compare the input_data against.
++
++    Returns:
++        float: A score between 0.0 and 1.0, where 1.0 means the strings are identical.
++    """
+
     input_data = "東京都渋谷区恵比寿1-1-1"
     database_data = "東京都渋谷区恵比寿1-1-1"
     expected_score = 1.0
